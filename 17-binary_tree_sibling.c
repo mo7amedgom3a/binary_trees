3de9b539a91774f08c28a811514d85b7ee0ebf66
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-01-05 06:03:57 +0200

    test

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 
-	if (node == node->parent->left)
+	if (node == node->arent->left)
 		return (node->parent->right);
 	return (node->parent->left);
 }
