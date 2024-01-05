f1fe53996cebae2d23e9e0642acd372e43f2d8c3
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-01-05 16:30:37 +0200

    test

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -15,23 +15,23 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 	maxlevel = binary_tree_height(tree) + 1;
 
 	for (level = 1; level <= maxlevel; level++)
-		printCurrentLevel(tree, func, level);
+		btlo_helper(tree, func, level);
 }
 
 /**
- * printCurrentLevel - goes through a binary tree
+ * btlo_helper - goes through a binary tree using post-order traverse
  * @tree: tree to traverse
  * @func: pointer to a function to call for each node
  * @level: the level of the tree to call func upon
  */
-void printCurrentLevel(const binary_tree_t *tree, void (*func)(int), size_t level)
+void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level)
 {
 	if (level == 1)
 		func(tree->n);
 	else
 	{
-		printCurrentLevel(tree->left, func, level - 1);
-		printCurrentLevel(tree->right, func, level - 1);
+		btlo_helper(tree->left, func, level - 1);
+		btlo_helper(tree->right, func, level - 1);
 	}
 }
 
