ee85edec0e2c1cbf8ae74ab5544ff01218886a22
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-01-05 04:59:37 +0200

    test

--- binary_trees.h
+++ binary_trees.h
@@ -51,5 +51,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
-int pow_recursion(int x, int y);
+int _pow_recursion(int x, int y);
 #endif
