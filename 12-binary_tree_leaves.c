7a8ac0c4d8ae476f903e5c117b3d6cf5a1d92a68
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-01-05 03:46:25 +0200

    test

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,12 +1,22 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: tree to count the leaves from
+ *
+ * Return: number of leaves
+ *         0 if tree is NULL
+ */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    int counter = 0;
-    if(!tree)
-        return 0;
-    if (!tree->left && !tree->right)
-        counter++;
-    binary_tree_leaves(tree->left);
-    binary_tree_leaves(tree->right);
-    return counter;
+	int counter = 0;
+
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		counter++;
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right)
+	+ counter);
 }
