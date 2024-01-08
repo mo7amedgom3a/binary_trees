7e41a85fd29d0b9fea249324cd8aef6f2c4f08d7
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-01-08 04:03:12 +0200

    test

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_left - Function that  rotates left the binary tree.
+ * binary_tree_rotate_left - Function that  rotates left RR rotation.
  * @tree: The root node of the three
  * Return: Pointer node of the new node
  */
