8e372bac4038ce13c4f205ae76331e10e67a91fa
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-01-08 02:16:29 +0200

    test

---
+++ 114-bst_remove2.c
@@ -0,0 +1,47 @@
+#include "binary_trees.h"
+
+/**
+ * bst_remove - Remove a node from a BST tree.
+ * @root: Root of the tree.
+ * @value: Node with this value to remove.
+ * Return: The tree after removing the desired value.
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	if (!root)
+		return NULL;
+
+	if (value < root->n)
+		root->left = bst_remove(root->left, value);
+	else if (value > root->n)
+		root->right = bst_remove(root->right, value);
+	else
+	{
+		// Node found, handle removal
+		if (root->left == NULL)
+		{
+			bst_t *temp = root->right;
+			free(root);
+			return temp;
+		}
+		else if (root->right == NULL)
+		{
+			bst_t *temp = root->left;
+			free(root);
+			return temp;
+		}
+
+		// Node has two children: find in-order successor
+		bst_t *successor = root->right;
+		while (successor->left != NULL)
+			successor = successor->left;
+
+		// Replace the node's value with the in-order successor's value
+		root->n = successor->n;
+
+		// Remove the in-order successor
+		root->right = bst_remove(root->right, successor->n);
+	}
+
+	return root;
+}
