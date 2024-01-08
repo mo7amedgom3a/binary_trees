3b2c1b075104d388a59d2fcffcbe721acf69e722
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-01-08 02:12:14 +0200

    test

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,54 +1,47 @@
 #include "binary_trees.h"
 
 /**
- * bst_remove - remove a node from a BST tree
- * @root: root of the tree
- * @value: node with this value to remove
- * Return: the tree changed
+ * bst_remove - Remove a node from a BST tree.
+ * @root: Root of the tree.
+ * @value: Node with this value to remove.
+ * Return: The tree after removing the desired value.
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-	bst_t *node = NULL;
-
 	if (!root)
 		return (NULL);
-	if (value > root->n)
-		root->right = bst_remove(root->right, value);
-	else if (value < root->n)
+
+	if (value < root->n)
 		root->left = bst_remove(root->left, value);
+	else if (value > root->n)
+		root->right = bst_remove(root->right, value);
 	else
 	{
 		if (root->left == NULL)
 		{
-			node = root->right;
+			bst_t *temp = root->right;
+
 			free(root);
-			return (node);
+			return (temp);
 		}
-		if (root->right == NULL)
+		else if (root->right == NULL)
 		{
-			node = root->left;
+			bst_t *temp = root->left;
+
 			free(root);
-			return (node);
+			return (temp);
 		}
-		node = minValueNode(root->right);
-		root->n = node->n;
-		root->right = bst_remove(root->right, node->n);
+
+		bst_t *successor = root->right;
+
+		while (successor->left != NULL)
+			successor = successor->left;
+
+		root->n = successor->n;
+
+		/*Remove the in-order successor*/
+		root->right = bst_remove(root->right, successor->n);
 	}
+
 	return (root);
-}
-
-/**
- * minValueNode - get the next successor
- * @root: the right child
- * Return: the min value of this tree
- */
-bst_t *minValueNode(bst_t *root)
-{
-	bst_t *curr = root;
-
-	while (curr->left)
-	{
-		curr = curr->left;
-	}
-	return (curr);
 }
